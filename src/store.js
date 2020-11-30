cce6a557181d206dce70a5561d7919689b526bce
md2eoseo <md2eoseo@gmail.com>
2020-11-30 17:34:58 +0900

    redux toolkit: createSlice

--- src/store.js
+++ src/store.js
@@ -1,23 +1,25 @@
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
-
-export const addTodo = createAction("ADD");
-export const toggleDone = createAction("DONE");
-export const deleteTodo = createAction("DELETE");
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 let id = 0;
 
-const reducer = createReducer([], {
-  [addTodo]: (todos, { payload }) => {
-    todos.push({ id: id++, text: payload.text.trim(), done: false });
+const toDos = createSlice({
+  name: "todosReducer",
+  initialState: [],
+  reducers: {
+    addTodo: (todos, { payload }) => {
+      todos.push({ id: id++, text: payload.text.trim(), done: false });
+    },
+    toggleDone: (todos, { payload }) =>
+      todos.map((todo) =>
+        todo.id === payload.id ? { ...todo, done: !todo.done } : todo
+      ),
+    deleteTodo: (todos, { payload }) =>
+      todos.filter((todo) => todo.id !== payload.id),
   },
-  [toggleDone]: (todos, { payload }) =>
-    todos.map((todo) =>
-      todo.id === payload.id ? { ...todo, done: !todo.done } : todo
-    ),
-  [deleteTodo]: (todos, { payload }) =>
-    todos.filter((todo) => todo.id !== payload.id),
 });
 
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
+
+export const { addTodo, toggleDone, deleteTodo } = toDos.actions;
 
 export default store;
