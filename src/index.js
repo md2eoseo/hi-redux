6dd00e454ba706de9b3567efef5b3e64ca05dcfb
md2eoseo <md2eoseo@gmail.com>
2020-11-30 11:32:54 +0900

    counter with vanilla redux

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const add = document.querySelector("#add");
+const minus = document.querySelector("#minus");
+const span = document.querySelector("span");
+
+span.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  span.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
