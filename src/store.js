24a349b9bd5a0c494cd54af855ce2721c5553125
md2eoseo <md2eoseo@gmail.com>
2020-11-30 17:03:31 +0900

    redux toolkit: createReducer shorter than prev

--- src/store.js
+++ src/store.js
@@ -1,25 +1,21 @@
 import { createAction, createReducer } from "@reduxjs/toolkit";
 import { createStore } from "redux";
 
-const ADD = "ADD";
-const DONE = "DONE";
-const DELETE = "DELETE";
-
-export const addTodo = createAction(ADD);
-export const toggleDone = createAction(DONE);
-export const deleteTodo = createAction(DELETE);
+export const addTodo = createAction("ADD");
+export const toggleDone = createAction("DONE");
+export const deleteTodo = createAction("DELETE");
 
 let id = 0;
 
 const reducer = createReducer([], {
-  ADD: (todos, { payload }) => {
+  [addTodo]: (todos, { payload }) => {
     todos.push({ id: id++, text: payload.text.trim(), done: false });
   },
-  DONE: (todos, { payload }) =>
+  [toggleDone]: (todos, { payload }) =>
     todos.map((todo) =>
       todo.id === payload.id ? { ...todo, done: !todo.done } : todo
     ),
-  DELETE: (todos, { payload }) =>
+  [deleteTodo]: (todos, { payload }) =>
     todos.filter((todo) => todo.id !== payload.id),
 });
 
